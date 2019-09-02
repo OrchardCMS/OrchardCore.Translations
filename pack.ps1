6f46a5e603ae1807971d8f051e968644304673e2
Hisham Bin Ateya <hishamco_2007@yahoo.com>
2019-09-02 17:53:17 +0300

    Use nuget in powershell

--- pack.ps1
+++ pack.ps1
@@ -5,16 +5,19 @@ $packageExtension = ".nupkg"
 $packageNamePrefix = "OrchardCore.Translations."
 $packageVersionNumber = "1.0.0-beta-$env:BuildNumber"
 
-$packageSpecExtension = ".nuspec"
-$packageSpecTemplate = "_template$packageSpecExtension"
+$packageSpecExtension = "nuspec"
+$packageSpecTemplate = "_template.$packageSpecExtension"
 
 $compressedFileExtension = ".zip"
 
+$pkgDescription = "Orchard Core translation for '{0}' culture"
+$pkgAuthor = "The Orchard Team"
+
 function createNuGetPackage([string]$culture)
 {
-    $packageName = $packageNamePrefix + $culture;
-    $NuGetSpecFileName = $packageName + $packageSpecExtension;
-    $NuGetSpecFilePath = [IO.Path]::Combine($PWD, $localizationFolder, $culture, $NuGetSpecFileName);
+    $packageName = $packageNamePrefix + $culture
+    $NuGetSpecFileName = "$packageName.$packageSpecExtension"
+    $NuGetSpecFilePath = [IO.Path]::Combine($PWD, $localizationFolder, $culture, $NuGetSpecFileName)
 
     $PackageFolderPath = [IO.Path]::Combine($artifactsFolder, "$packageName.$packageVersionNumber")
     $contentFolderPath = [IO.Path]::Combine($PackageFolderPath, "content")
@@ -30,7 +33,7 @@ function createNuGetPackage([string]$culture)
     $cultureFolderPath = [IO.Path]::Combine($localizationFolderPath, $culture)
     Copy-Item -Path $cultureFolder -Destination $cultureFolderPath -Recurse
 
-    prepareNuGetSpec $culture;
+    prepareNuGetSpec $culture
 
     $packageFullName = "$packageName.$packageVersionNumber$packageExtension"
     $packagePath = [IO.Path]::Combine($artifactsFolder, $packageFullName)
@@ -38,9 +41,11 @@ function createNuGetPackage([string]$culture)
     $compressedCulturePath = [IO.Path]::Combine($artifactsFolder, $compressedCultureFileName)
     $cultureFolder = [IO.Path]::Combine($PWD, $localizationFolder, $culture);
 
-    Compress-Archive -Path $PackageFolderPath -DestinationPath "$PackageFolderPath$compressedFileExtension" -CompressionLevel Optimal -Update
-    Rename-Item "$PackageFolderPath.zip" $packageFullName
-    Remove-Item -Path $PackageFolderPath -Recurse
+    #Compress-Archive -Path $PackageFolderPath -DestinationPath "$PackageFolderPath$compressedFileExtension" -CompressionLevel Optimal -Update
+    #Rename-Item "$PackageFolderPath.zip" $packageFullName
+    #Remove-Item -Path $PackageFolderPath -Recurse
+
+    nuget.exe pack $NuGetSpecFilePath
 }
 
 function prepareNuGetSpec([string]$culture)
@@ -48,14 +53,15 @@ function prepareNuGetSpec([string]$culture)
     $packageFolderPath = [IO.Path]::Combine($artifactsFolder, "$packageName.$packageVersionNumber")
 
     $packageName = $packageNamePrefix + $culture
-    $NuGetSpecFileName = $packageName + $packageSpecExtension
+    $NuGetSpecFileName = "$packageName.$packageSpecExtension"
     $NuGetSpecFilePath = [IO.Path]::Combine($PWD, $packageFolderPath, $NuGetSpecFileName)
 
     $nugetSpec = [xml](Get-Content -Path $packageSpecTemplate)
     $metadata = $nugetSpec.package.metadata
     $metadata.id = $packageName
     $metadata.version = $packageVersionNumber
-    $metadata.description = "Orchard Core translation for '$culture' culture"
+    $metadata.description = [String]::Format($pkgDescription, $culture)
+    $metadata.authors = $pkgAuthor
     $nugetSpec.Save($NuGetSpecFilePath)
 }
 
